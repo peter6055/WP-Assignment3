5fd074c3d47279873dc15f4844663e8319a24b80
SHIHMING LIU s3789585 <s3789585@student.rmit.edu.au>
2021-11-01 12:14:55 +1100

    File/Environment set up

---
+++ team.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home | Life - Living It Fully Everyday</title>
+    <link rel="stylesheet" href="assets/team.css">
+
+    <!-- Google font API-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css?family=Noto Sans" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<div class="container">
+    <h1>Meet Our Team</h1>
+</div>
+<div class="container">123</div>
+
+</body>
+</html>
