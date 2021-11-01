e6d35dfecf5a88ad6c2d1aad7778397dc57b429e
SHIHMING LIU s3789585 <s3789585@student.rmit.edu.au>
2021-11-01 14:22:16 +1100

    task2 done

--- team.php
+++ team.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Home | Life - Living It Fully Everyday</title>
+    <title>Meet Our Team</title>
     <link rel="stylesheet" href="assets/team.css">
 
     <!-- Google font API-->
@@ -14,10 +14,65 @@
 </head>
 
 <body>
-<div class="container">
-    <h1>Meet Our Team</h1>
-</div>
-<div class="container">123</div>
+    <div class="wapper">
+        <div class="container d-flex-cus" id="bg1">
+            <h1>Meet Our Team</h1>
+        </div>
+
+        <div class="container d-grid-cus">
+            <div class="d-grid-item-cus" id="people-item1">
+                <div class="profile-img" style="background-image: url('assets/images/p2.svg'); background-color: #68dcd145;"></div>
+                <div class="profile-name">Lisa Maheta</div>
+                <div class="profile-role" style="border-color: #68dbd1;">MARKETING</div>
+                <div class="profile-intro">Graduated with the Master of Quality Management from The Hong Kong Polytechnic University, Lisa was the Senior Specialist of Brand and Marketing of Alibaba, and now the Founder & CEO of KaDa Story.</div>
+                <div class="profile-social-media">
+                    <a href="https://www.facebook.com/RMITuniversity/" target="_blank"><img src="assets/images/68dbd1/facebook.png" alt="facebook" width="20px"></a>
+                    <a href="https://twitter.com/rmit" target="_blank"><img src="assets/images/68dbd1/twitter.png" alt="twitter" width="20px"></a>
+                    <a href="https://www.rmit.edu.au/" target="_blank"><img src="assets/images/68dbd1/google.png" alt="google" width="20px"></a>
+                    <a href="https://au.linkedin.com/school/rmit-university/" target="_blank"><img src="assets/images/68dbd1/linkedin.png" alt="linkedin" width="20px"></a>
+                </div>
+            </div>
+
+            <div class="d-grid-item-cus" id="people-item2">
+                <div class="profile-img" style="background-image: url('assets/images/p1.svg'); background-color: #91d6a652;"></div>
+                <div class="profile-name">John Meker</div>
+                <div class="profile-role" style="border-color: #90d6a6;">DESIGNER</div>
+                <div class="profile-intro">John is the Managing Director of Seer Asset Management whose focus is to develop and manage wholesale investment funds, investing in alternative assets and sustainable and responsible global equities.</div>
+                <div class="profile-social-media"></div>
+                <div class="profile-social-media">
+                    <a href="https://www.facebook.com/RMITuniversity/" target="_blank"><img src="assets/images/90d6a6/facebook.png" alt="facebook" width="20px"></a>
+                    <a href="https://twitter.com/rmit" target="_blank"><img src="assets/images/90d6a6/twitter.png" alt="twitter" width="20px"></a>
+                    <a href="https://www.rmit.edu.au/" target="_blank"><img src="assets/images/90d6a6/google.png" alt="google" width="20px"></a>
+                    <a href="https://au.linkedin.com/school/rmit-university/" target="_blank"><img src="assets/images/90d6a6/linkedin.png" alt="linkedin" width="20px"></a>
+                </div>
+            </div>
+
+            <div class="d-grid-item-cus" id="people-item3">
+                <div class="profile-img" style="background-image: url('assets/images/p3.svg'); background-color: #f0826a3d;"></div>
+                <div class="profile-name">Vin Deasel</div>
+                <div class="profile-role" style="border-color: #f0826a;">PRODUCER</div>
+                <div class="profile-intro">Siimon Reynolds is a serial entrepreneur with over 3 decades of experience building successful businesses. He co- founded the 15th largest marketing communications business in the world, with 6000 employees in 14 countries.</div>
+                <div class="profile-social-media">
+                    <a href="https://www.facebook.com/RMITuniversity/" target="_blank"><img src="assets/images/f0826a/facebook.png" alt="facebook" width="20px"></a>
+                    <a href="https://twitter.com/rmit" target="_blank"><img src="assets/images/f0826a/twitter.png" alt="twitter" width="20px"></a>
+                    <a href="https://www.rmit.edu.au/" target="_blank"><img src="assets/images/f0826a/google.png" alt="google" width="20px"></a>
+                    <a href="https://au.linkedin.com/school/rmit-university/" target="_blank"><img src="assets/images/f0826a/linkedin.png" alt="linkedin" width="20px"></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <footer class="container" id="footer">
+        <div id="left">
+            Copyright 2021 by Peter Liu. All Rights Reserved.<br>
+
+        </div>
+
+        <div id="right">
+            <?php echo 'This webpage was last modified by LIU SHIH MING on '.date('Y-m-d H:i:s', filemtime(__FILE__));?>
+        </div>
+    </footer>
 
 </body>
 </html>
